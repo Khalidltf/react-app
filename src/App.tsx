6937a3b620a627b6b5a741bac43934b85f338705
khalidlotfi <khalidlotfi2005@gmail.com>
2023-11-13 16:08:35 +0100

    Building a Button Component & Showing an Alert
    Signed-off-by: khalidlotfi <khalidlotfi2005@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import Button from "./components/Button";
 import Alert from "./components/Alert";
+import { useState } from "react";
 
 const App = () => {
+  const [alert, setAlert] = useState(false)
+
   return (
     <div>
-      <Alert>
-        <span>Hello mum!</span>
-      </Alert>
+      {alert && <Alert onClose={() => setAlert(false)}>My alert</Alert>}
+      <Button
+        color = 'secondary'
+        onClick={() => setAlert(true)}>
+        Click
+      </Button>
     </div>
   );
 };
